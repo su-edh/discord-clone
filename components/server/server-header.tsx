9fdb97a88df868ab6e15a9b81734625383606caa
su-edh <lucifer.logan0007@gmail.com>
2024-10-27 02:14:01 +0530

    add Delete and Leave server modal/functionality

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -89,6 +89,7 @@ export const ServerHeader = ({
                 )}
                 {isAdmin && (
                     <DropdownMenuItem
+                        onClick={()=> onOpen("deleteServer", { server })}
                         className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
                     >
                         Delete Server
@@ -97,6 +98,7 @@ export const ServerHeader = ({
                 )}
                 {!isAdmin && (
                     <DropdownMenuItem
+                        onClick={()=>onOpen("leaveServer", {server})}
                         className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
                     >
                         Leave Server
